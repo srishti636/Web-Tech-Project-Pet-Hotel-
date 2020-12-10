41b85b8305ca0907aa0d7c8b9ba679ff8688fed5
STUTI-01 <59913235+STUTI-01@users.noreply.github.com>
2020-12-10 14:32:50 +0530

    rent page modified

--- pethotel/client/src/components/views/RentPage/RentPage.js
+++ pethotel/client/src/components/views/RentPage/RentPage.js
@@ -1,90 +1,94 @@
-import React from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBInput, MDBIcon, MDBBtn } from 'mdbreact';
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import { withRouter } from 'react-router-dom'
+import "react-datepicker/dist/react-datepicker.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+class RentPage extends Component {
 
-const RentPage = () => {
-  return (
-    <MDBContainer>
-      <MDBRow>
-        <MDBCol md='6'>
-          <MDBCard>
-            <MDBCardBody className='mx-4'>
-              <div className='text-center'>
-                <h3 className='pink-text mb-5'>
-                  <strong>Sign up</strong>
-                </h3>
-              </div>
-              <MDBInput label='Your email' group type='text' validate />
-              <MDBInput label='Your password' group type='password' validate />
-              <div className='md-form pb-3'>
-                <div className='form-check my-4'>
-                  <MDBInput
-                    label={
-                      <>
-                        Accept&nbsp;
-                        <a href='!#' className='blue-text'>
-                          the Terms and Conditions
-                        </a>
-                      </>
-                    }
-                    type='checkbox'
-                    id='checkbox1'
-                  />
-                </div>
-              </div>
-              <MDBRow className='d-flex align-items-center mb-4'>
-                <MDBCol md='6' className='text-center'>
-                  <MDBBtn className='z-depth-1' color='pink' rounded block>
-                    Sign up
-                  </MDBBtn>
-                </MDBCol>
-                <MDBCol md='6'>
-                  <p className='font-small grey-text d-flex justify-content-end'>
-                    Have an account?
-                    <a href='#!' className='blue-text ml-1'>
-                      Log in
-                    </a>
-                  </p>
-                </MDBCol>
-              </MDBRow>
-            </MDBCardBody>
-            <div className='footer pt-3 mdb-color lighten-3'>
-              <MDBRow className='d-flex justify-content-center'>
-                <p className='font-small white-text mb-2 pt-3'>
-                  or Sign up with
-                </p>
-              </MDBRow>
-              <MDBRow className='mt-2 mb-3 d-flex justify-content-center'>
-                <a href='#!' className='fa-lg p-2 m-2 fb-ic'>
-                  <MDBIcon
-                    icon='facebook-f'
-                    fab
-                    size='lg'
-                    className='white-text'
-                  ></MDBIcon>
-                </a>
-                <a href='#!' className='fa-lg p-2 m-2 tw-ic'>
-                  <MDBIcon
-                    fab
-                    icon='twitter'
-                    size='lg'
-                    className='white-text'
-                  ></MDBIcon>
-                </a>
-                <a href='#!' className='fa-lg p-2 m-2 gplus-ic'>
-                  <MDBIcon
-                    fab
-                    icon='google-plus-g'
-                    size='lg'
-                    className='white-text'
-                  ></MDBIcon>
-                </a>
-              </MDBRow>
-            </div>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-  );
-};
+  constructor (props) {
+    super(props)
+    this.state = {
+      startDate: new Date()
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    })
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+    var dt = new Date();
+    var price = 0;
+    var daydiff = ((this.state.startDate - dt)/86400000)+0.1;
+    if(daydiff >=0)
+    {
+        price = daydiff*15;
+        console.log(daydiff);
+        document.write("The total price is " + price + " $USD");
+
+    }
+  }
+ 
+  render() {
+    return (
+      <form onSubmit={ this.onFormSubmit }>
+        <div style = {{ maxWidth: '700px', margin: '10rem auto' }}className="form-group">
+        <label>Pet Name</label>
+        <br/>
+        <input type = "text" placeholder = "Pet Name"></input>
+        <br/>
+        <br/>
+        <label>Pet Age</label>
+        <br/>
+        <input type = "text" placeholder = "Pet Age"></input>
+        <br></br>
+        <br/>
+        <br></br>
+        <br></br>
+        <label>Checkout Date</label>
+        <br/>
+        <DatePicker
+              selected={ this.state.startDate }
+              onChange={ this.handleChange }
+              name="startDate"
+              dateFormat="dd/MM/yyyy"
+          />
+          <br>
+          </br>
+          <br></br>
+        <label>Customer Name</label>
+        <br/>
+        <input type = "text" placeholder = "Customer name"></input>
+        <br/>
+        <br></br>
+        <label>Customer e-mail id</label>
+        <br/>
+        <input type = "text" placeholder = "Customer email id"></input>
+        <br></br>
+        <br></br>
+        <label>Contact</label>
+        <br/>
+        <input type = "text" placeholder = "Contact"></input>
+        <br></br>
+        <br>
+        </br>
+        <label>Other Details</label>
+        <br/>
+        <input type = "textarea" placeholder = "Other Details"></input>
+        <br/>
+        <br/>
+       
+          <button className="btn btn-primary">Submit Data</button>
+        </div>
+      </form>
+    );
+  }
+  
+}
 
 export default RentPage;
